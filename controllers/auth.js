b8bc0c13d8afb5a22c89227cabe88f676ec8ff5e
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-12-14 12:25:22 -0400

    Login del usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,15 +36,35 @@ const crearUsuario = async (req, res = response) => {
   }
 };
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async (req, res = response) => {
   const { email, password } = req.body;
 
-  res.status(201).json({
-    ok: true,
-    msg: "login",
-    email,
-    password,
-  });
+  try {
+    let usuario = await Usuario.findOne({ email });
+    //console.log(usuario);
+
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario no existe con ese email",
+      });
+    }
+    //Confirmar los passwords
+    const validPassword = bcrypt.compareSync(password, usuario.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Password incorrecto",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
 };
 
 module.exports = {
