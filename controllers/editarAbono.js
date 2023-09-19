b7e4efcb2e4b7bbfe4156fb8f2c88b2c4a92dbd1
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-09-19 10:33:51 -0400

    Agregando Logica para hacer filtrado de abonos por recepcionista en fecha actual o con parametro de fecha opcional

--- controllers/editarAbono.js
+++ controllers/editarAbono.js
@@ -1,5 +1,7 @@
 const { response } = require("express");
 const Abono = require("../models/ControlCuenta");
+const Registro = require("../models/Registro");
+const { getRegistros, getRegistroById } = require("./registro");
 
 const getComandasByReservaId = async (req, res = response) => {
   const idReserva = req.params.idReserva;
@@ -50,32 +52,108 @@ const getAllAbonos = async (req, res = response) => {
     });
   }
 };
+//*------adicionando filtrado y reservas
+// const getAbonosByRecepcionistaId = async (req, res = response) => {
+//   const idRecepcionista = req.params.idRecepcionista;
+//   const fechaConsulta = req.query.fecha;
+//   const fechaConsultaObj = fechaConsulta ? new Date(fechaConsulta) : new Date();
 
+//   try {
+//     // Busca los abonos realizados por el recepcionista en las últimas 24 horas
+//     const abonos = await Abono.find({
+//       idRecepcionista: idRecepcionista,
+//       fechaActual: {
+//         $gte: fechaConsultaObj, // Filtra por fecha igual o posterior a la fecha de consulta
+//         $lt: new Date(fechaConsultaObj.getTime() + 24 * 60 * 60 * 1000), // Hasta el final del día
+//       },
+//     });
+
+//     if (abonos.length === 0) {
+//       return res.status(404).json({
+//         ok: false,
+//         msg: "No se encontraron abonos para el recepcionista en las últimas 24 horas",
+//       });
+//     }
+
+//     // Ahora, para cada abono, busca la reserva correspondiente por idReserva
+
+//     const abonosConReservas = await Promise.all(
+//       abonos.map(async (abono) => {
+//         const reserva = await Registro.findById(abono.idReserva);
+//         return { ...abono.toObject(), reserva }; // Combina el abono con la reserva
+//       })
+//     );
+//     res.json({
+//       ok: true,
+//       abonos: abonosConReservas,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({
+//       ok: false,
+//       msg: "Hable con el administrador. Problema al obtener los abonos por ID de recepcionista",
+//     });
+//   }
+// };
 const getAbonosByRecepcionistaId = async (req, res = response) => {
   const idRecepcionista = req.params.idRecepcionista;
+  const fechaConsulta = req.query.fecha; // Puede ser la fecha actual o una fecha específica
 
-  // Obtén la fecha actual y calcula la fecha hace 24 horas
-  const fechaActual = new Date();
-  const fechaLimite = new Date(fechaActual);
-  fechaLimite.setHours(fechaActual.getHours() - 24);
+  let consultaFecha = {};
+
+  if (fechaConsulta) {
+    // Si se proporciona una fecha, consulta solo para esa fecha
+    const fechaConsultaObj = new Date(fechaConsulta);
+    fechaConsultaObj.setHours(0, 0, 0, 0); // Establece la fecha al inicio del día
+    const fechaFinDia = new Date(
+      fechaConsultaObj.getTime() + 24 * 60 * 60 * 1000
+    ); // Fin del día
+
+    consultaFecha = {
+      fechaActual: {
+        $gte: fechaConsultaObj,
+        $lt: fechaFinDia,
+      },
+    };
+  } else {
+    // Si no se proporciona una fecha, consulta para el día actual
+    const fechaActual = new Date();
+    fechaActual.setHours(0, 0, 0, 0); // Establece la fecha al inicio del día
+    const fechaFinDia = new Date(fechaActual.getTime() + 24 * 60 * 60 * 1000); // Fin del día
+
+    consultaFecha = {
+      fechaActual: {
+        $gte: fechaActual,
+        $lt: fechaFinDia,
+      },
+    };
+  }
 
   try {
-    // Busca los abonos realizados por el recepcionista en las últimas 24 horas
+    // Busca los abonos realizados por el recepcionista en la fecha especificada o el día actual
     const abonos = await Abono.find({
       idRecepcionista: idRecepcionista,
-      fechaActual: { $gte: fechaLimite }, // Filtra por fecha en las últimas 24 horas
+      ...consultaFecha, // Agrega la condición de fecha según lo configurado
     });
 
     if (abonos.length === 0) {
       return res.status(404).json({
         ok: false,
-        msg: "No se encontraron abonos para el recepcionista en las últimas 24 horas",
+        msg: "No se encontraron abonos para el recepcionista en la fecha especificada",
       });
     }
 
+    // Ahora, para cada abono, busca la reserva correspondiente por idReserva
+
+    const abonosConReservas = await Promise.all(
+      abonos.map(async (abono) => {
+        const reserva = await Registro.findById(abono.idReserva);
+        return { ...abono.toObject(), reserva }; // Combina el abono con la reserva
+      })
+    );
     res.json({
       ok: true,
-      abonos,
+      abonos: abonosConReservas,
     });
   } catch (error) {
     console.log(error);
@@ -85,7 +163,6 @@ const getAbonosByRecepcionistaId = async (req, res = response) => {
     });
   }
 };
-
 module.exports = {
   getAbonosByRecepcionistaId,
   getComandasByReservaId,
